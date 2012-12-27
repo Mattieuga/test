8fc59853ccce98887b1dcc103a8ee493dc9d2a19
Mattieu Gamache-Asselin <matt@parse.com>
2012-12-27 11:30:32 -0800

    seperate internal js files

---
+++ internal_parse_subclasses.js
@@ -0,0 +1,15 @@
+    // We subclass Parse.Object so we can introspect the objects the 
+    // user creates by saving them all in a global array. Pretty hacky,
+    // but this entire codecademy thing is pretty hacky o_O
+    window._parsePostObjects = [];
+    Parse.Object.extend('Post', {
+        initialize: function() {
+            window._parsePostObjects.push(this);
+        }
+    });
+    window._parseCommentObjects = [];
+    Parse.Object.extend('Comment', {
+        initialize: function() {
+            window._parseCommentObjects.push(this);
+        }
+    });
